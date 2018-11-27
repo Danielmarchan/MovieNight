b04b610054db4097a333b3b64bfe496d0e8afeac
EC2 Default User <ec2-user@ip-172-31-36-166.us-east-2.compute.internal>
2018-11-27 15:18:40 +0000

    Updates

---
+++ precache-manifest.843a9337a9b6f3ae695ddbfaf7a33007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movieNight/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4c59729add0e278abf6f",
+    "url": "/movieNight/static/js/runtime~main.4c59729a.js"
+  },
+  {
+    "revision": "e0af26eb29e9248141bd",
+    "url": "/movieNight/static/js/main.e0af26eb.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/movieNight/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "e0af26eb29e9248141bd",
+    "url": "/movieNight/static/css/main.39f609dd.chunk.css"
+  },
+  {
+    "revision": "7b20feaf1daaab923ac527dbfb698790",
+    "url": "/movieNight/index.html"
+  }
+];
